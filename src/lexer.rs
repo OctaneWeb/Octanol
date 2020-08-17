bda37090620db30de99ab7c4dbf8830377d34000
neverRare <35887047+neverRare@users.noreply.github.com>
2020-08-17 09:38:46 +0800

    restructure nodes

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,8 +1,43 @@
-pub enum HtmlToken<'a> {
-    Less,
-    Greater,
-    Slash,
-    Text,
-    Equal,
-    Str(&'a str),
+enum Token<'a> {
+    Text(&'a str),
+    Tag(Tag<'a>),
+    Placeholder(&'a str),
+    For(For<'a>),
+    If(If<'a>),
+}
+type Tokens<'a> = Vec<Token<'a>>;
+struct Tag<'a> {
+    name: &'a str,
+    slash: Slash,
+    attrs: Vec<Attr<'a>>,
+}
+struct Attr<'a> {
+    name: &'a str,
+    value: AttrValue<'a>,
+}
+enum AttrValue<'a> {
+    Placeholder(&'a str),
+    Value(&'a str),
+}
+enum Slash {
+    Left,
+    Right,
+    None,
+}
+struct For<'a> {
+    var: &'a str,
+    expr: &'a str,
+    body: Tokens<'a>,
+}
+struct If<'a> {
+    condition: Condition<'a>,
+    body: Tokens<'a>,
+    else_part: Option<Box<If<'a>>>,
+}
+enum Condition<'a> {
+    Bool(&'a str),
+    Let {
+        pattern: &'a str,
+        scrutinee: &'a str,
+    },
 }
